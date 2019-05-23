43d93a81ec8f649b50aea8f2907e24c40907074b
Jonathan Oaklyn <jonathan.oaklyn@busyhuman.io>
2019-05-23 10:49:57 -0600

    Deploys and additional structures

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# rabbit-hutch
-A wrapper to simplify dealing with multiple RabbitMQ queues
+# rabbitmq-hutch
+A wrapper to simplify dealing with multiple RabbitMQ queues or a continous stream of tasks
+
+## Why Use Rabbit Hutch?
+
+- Timeout handling to prevent tasks that never finish from freezing your application
+- Simplified interaction with Rabbit, just provide the url, queue name and handler
+- Improved console logging makes it more clear which tasks are running
+
+## Installation
+
+    // For npm
+    npm install rabbitmq-hutch --save
+
+    // For yarn
+    yarn add rabbitmq-hutch
+
+## Usage
+
+    const RabbitHutch = require("rabbitmq-hutch");
+    var hutch = new RabbitHutch("amqp://...");
+    hutch.connect()
+        .then(channel => {
+            hutch.consumeQueue("my-queue", { timelimit: 1000 * 60 * 3 }, function(data, msg, ack, nack, cancelTimeout) {
+                // Do some work
+                // ...
+
+                // Acknowledge once you've finished
+                ack();
+            })
+        });
+
+
+## Optional Express Integration
+You may want to be able to run your functions through a REST call, maybe for testing, or maybe for other purposes.
+
+If you are using express, or an express-like interface, you can pass it in to get URL endpoints automatically generated for each queue.
+
+    var hutch = new RabbitHutch("amqp://...", { expressApp: app });
+    hutch.connect()
+        .then(() => {
+            hutch.consumeQueue("my-queue", ...)
+        });
+
+    // Now you can use: manually send a message to the queue like so:
+    // POST http://<YOUR-DOMAIN>/consume/my-queue
+
+With the express integration, Hutch tries to mimic the normal message queue process as closely as possible, so it is a great way to test your queue consumer functions without having to run the full pipeline.
---
+++ index.js
@@ -0,0 +1,205 @@
+const amqplib = require("amqplib/callback_api");
+
+const LOGGER = {
+    originalConsoleLog: console.log,
+    originalConsoleWarn: console.warn,
+    queueName: 'no-queue',
+    messageId: null,
+    NO_QUEUE: "no-queue",
+    log() {
+        var args = [];
+        if(LOGGER.messageId) {
+            args.push(`MQ[${LOGGER.queueName}:${LOGGER.messageId}]`);
+        } else if(LOGGER.queueName) {
+            args.push(`MQ[${LOGGER.queueName}]`);
+        } else {
+            args.push(`MQ`);
+        }
+
+        for(var i = 0; i < arguments.length; i++) {
+            args.push(arguments[i]);
+        }
+        return LOGGER.originalConsoleLog.apply(console, args);
+    }
+};
+
+/**
+ * @class RabbitHutch
+ * @param {*} url - RabbitMQ endpoint URL
+ * @param {*} options - channel, expressApp, prefetchLimit
+ */
+const Hutch = function(url, options) {
+    this.url = url;
+
+    this.channel            = options.channel;
+    this.expressApp         = options.expressApp;
+    this.prefetchLimit      = options.prefetch || 1;
+};
+
+/**
+ * (Optional) Helps to clearly identify the origin (task/queue) of all console.log messages
+ * Note that this will impact all of your console logging, so unless your application solely/primarily
+ * consumes queues you may not want to use this.
+ */
+Hutch.replaceStandardLogger =function() {
+   console.log = LOGGER.log;
+};
+
+Hutch.prototype = {
+    /**
+     * Connects to your RabbitMQ endpoint
+     */
+    connect: function() {
+        if(!this.channel) {
+            return new Promise((resolve, reject) => {
+                amqplib.connect(this.url, (err, conn) => {
+                    if(err) {
+                        console.warn("Failed to connect to Rabbit MQ");
+                        console.error(err);
+                        reject(err);
+                    } else {
+                        conn.createChannel((err, channel) => {
+                            if(err) {
+                                console.warn("Failed to create a Rabbit MQ channel");
+                                console.err(err);
+                                throw err;
+                            }
+                            this.channel = channel;
+                            this.channel.prefetch(this.prefetchLimit); // Don't dispatch another message until the worker is done with this one
+
+                            resolve(this.channel);
+                        });
+                    }
+                });
+            });
+        } else {
+            return Promise.resolve(this.channel);
+        }
+    },
+    /**
+     * Sends a message to the specified queue
+     * @param {*} queue 
+     * @param {*} options - channel
+     * @param {*} payload 
+     */
+    sendToOtherQueue: function( queue, options, payload ) {
+        var channel     = options.channel || this.channel;
+        var str         = JSON.stringify(payload);
+
+        console.log(`Sending to other queue ${queue}`, str);
+        channel.assertQueue(queue, { durable: true });
+        channel.sendToQueue(queue, Buffer.from(str));  
+    },
+    
+    /**
+     * Consumes a single message from a queue, one-at-a-time
+     * @param {*} queueName 
+     * @param {*} options - timelimit, channel
+     * @param {*} fn 
+     */
+    consumeQueue: function(queueName, options, fn) {
+        var timelimit   = options.timelimit;
+        var channel     = options.channel || this.channel;
+
+        if(timelimit === undefined || timelimit === null || isNaN(timelimit)) {
+            timelimit = 1000 * 60 * 5; // Default timelimit is 5 minutes;
+        }
+        var setupFn = function (obj, msg, completeAck, completeNack) {
+            var randomId = Math.random() * 1000;
+            LOGGER.queueName = queueName;
+            LOGGER.messageId = msg.deliveryTag || `R-${randomId}`;
+            
+            console.log(`---- START OF ${queueName} MESSAGE ----`);
+            console.log(JSON.stringify(obj));
+    
+            var finished = false;
+            var finishType = null;
+    
+            var timeout = setTimeout(() => {
+                if(!finished) {
+                    console.warn(`TIMED OUT`);
+                    finished = true;
+                    finishType = "timeout/nack";
+                    nack(true);
+                }
+            }, timelimit);
+    
+            var ack = () => {
+                if(!finished) {
+                    cancelTimeout();
+                    finished = true;
+                    finishType = "ack";
+                    console.log("---- FINISHED PROCESSING " + queueName + " MESSAGE ----");
+                    LOGGER.queueName = LOGGER.NO_QUEUE;
+                    LOGGER.messageId = null;
+                    return completeAck(msg);
+                } else {
+                    console.warn(`already ${finishType}, cannot ack`);
+                }
+            };
+    
+            var nack = (requeue = false) => {
+                if(!finished) {
+                    cancelTimeout();
+                    finished = true;
+                    finishType = "nack";
+                    console.log(`-- Failed`);
+                    LOGGER.queueName = LOGGER.NO_QUEUE;
+                    LOGGER.messageId = null;                
+                    return completeNack(msg, requeue);
+                } else if(finishType == "timeout/nack") {
+                    LOGGER.queueName = LOGGER.NO_QUEUE;
+                    LOGGER.messageId = null;                
+                    return completeNack(msg, requeue);
+                } else {
+                    console.warn(`already ${finishType}, cannot nack`);
+                }
+            };
+    
+            var cancelTimeout = () => {
+                if(timeout) {
+                    clearTimeout(timeout);
+                    timeout = null;
+                }
+            };
+    
+            // FUNCTION TO CALL
+            fn(obj, msg, ack, nack, cancelTimeout);
+        };
+        
+        // Setup a manual REST route for triggering the action, usually for testing
+        if(this.expressApp) {
+            this.expressApp.post(`/consume/${queueName}`, function(req, res) {
+                var obj = req.body;
+                var msg = {
+                    content: obj
+                };
+            
+                var completeAck = (msg) => res.status(200).send(msg);
+                var completeNack = (msg, requeue) => res.status(500).send(msg);
+            
+                setupFn(obj, msg, completeAck, completeNack);
+            });
+        }
+
+    
+    
+    
+        // Setup the default queue consumer
+        channel.assertQueue(queueName, { durable: true });
+        channel.consume(queueName, function(msg) {
+            var str = msg.content.toString();
+    
+            var completeAck = (msg) => channel.ack(msg);
+            var completeNack = (msg, requeue) => channel.nack(msg, false, requeue);
+    
+            // Deserialize
+            var obj = JSON.parse(str);
+      
+            setupFn(obj, msg, completeAck, completeNack);
+        }, { noAck: false });    
+    }
+};
+
+
+module.exports = Hutch;
